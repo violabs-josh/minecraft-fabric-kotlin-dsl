e73024bbcba3e3a1226fd769d9c15ca6b9e1958e
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2025-03-23 21:49:43 -0500

    Base generator for assets

--- build.gradle.kts
+++ build.gradle.kts
@@ -120,6 +120,135 @@ tasks.withType(JavaExec::class.java) {
 	}
 }
 
+tasks.register("configureAssets") {
+	val assets = modAssets("beginner") {
+		item("guidite_sword") {
+			parent {
+				itemUtility(Utility.HANDHELD)
+			}
+		}
+	}
+
+	assets.items().forEach(::convertItemIntoJsonFiles)
+}
+
+fun convertItemIntoJsonFiles(item: ModAssets.Item) {
+	val itemDetails = item.toItemDetailsJson()
+	println(itemDetails)
+	val itemModel = item.toItemModelJson()
+	println(itemModel)
+}
+
+fun modAssets(name: String, config: ModAssets.() -> Unit = {}): ModAssets {
+	val mod = ModAssets(name)
+	mod.apply(config)
+	return mod
+}
+
+class ModAssets(
+	private val modName: String
+) {
+	private val items: MutableList<Item> = mutableListOf()
+
+	fun item(name: String, config: Item.() -> Unit = {}) {
+		val item = Item(name).apply(config)
+
+		items.add(item)
+	}
+
+	fun items(): MutableList<Item> = items
+
+	sealed class Metadata(
+		val type: MinecraftModelType = MinecraftModelType.ITEM,
+		open val utility: Utility? = null
+	) {
+		class Item(override val utility: Utility? = null) : Metadata(MinecraftModelType.ITEM)
+		class Model(override val utility: Utility? = null) : Metadata(MinecraftModelType.MODEL)
+	}
+
+	inner class Item(
+		private var itemName: String,
+		private var metadata: Metadata = Metadata.Model(),
+		private var parent: Metadata? = null
+	) {
+		private val textures: MutableList<Texture> = mutableListOf()
+
+		fun parent(config: ParentMetadata.() -> Unit = {}) {
+			val parentMetadata = ParentMetadata().apply(config)
+
+			parent = parentMetadata.content
+		}
+
+		fun toItemDetailsJson(): String = """
+		|{
+		|  "model": {
+		|    "type": "${metadata.type}"
+		|    "model": "$modName:item/$itemName"
+		|  }
+		|}
+		""".trimMargin("|")
+
+		fun toItemModelJson(): String {
+			val parentMetadata = requireNotNull(parent) { "Parent metadata not set" }
+
+			if (textures.isEmpty()) {
+				textures.add(Texture(0, itemName))
+			}
+
+			val textureString = textures.joinToString(",") {
+				"\"layer${it.index}\": \"$modName:item/${it.name}\""
+			}
+
+			return """
+			|{
+			|  "parent": "${parentMetadata.type}/${parentMetadata.utility}",
+			|  "textures": {
+			|    $textureString
+			|  }
+			|}
+			""".trimMargin("|")
+		}
+
+		inner class ParentMetadata {
+			var content: Metadata? = null
+			fun itemUtility(utility: Utility? = null) = setOrThrowErrorIfExists(utility, Metadata::Item)
+			fun modelUtility(utility: Utility? = null) = setOrThrowErrorIfExists(utility, Metadata::Model)
+
+			private fun setOrThrowErrorIfExists(utility: Utility?, provider: (Utility?) -> Metadata) {
+				if (content != null) {
+					throw IllegalStateException("Metadata already set")
+				}
+				content = provider(utility)
+			}
+		}
+	}
+
+	class Texture(
+		var index: Int = 0,
+		var name: String? = null
+	)
+}
+
+enum class Utility {
+	HANDHELD,
+	GENERATED;
+
+	override fun toString(): String = name.lowercase()
+}
+
+enum class MinecraftModelType {
+	ITEM,
+	MODEL;
+
+	override fun toString(): String {
+		return "$TYPE_ORIGIN:${name.lowercase()}"
+	}
+
+	companion object {
+		const val TYPE_ORIGIN = "minecraft"
+	}
+}
+
 //// configure the maven publication
 //publishing {
 //	publications {
